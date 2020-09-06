a24c4fad72798a0dbe818a97bca3a3894fe48ad0
JasonZhongZexin <samsung@svf-tester.asia-east2-a.c.v2ray-project-267121.internal>
2020-09-06 09:41:27 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# SVF-Static-Lib
-A static library for the open source tool SVF. This library allow user to use llvm as a third party library.
+# SVF-npm
+SVF's npm support
--- llvm_install.sh
+++ llvm_install.sh
@@ -4,7 +4,7 @@ sysOS=`uname -s`
 MacLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-apple-darwin.tar.xz"
 UbuntuLLVM="https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-x86_64-linux-gnu-ubuntu-18.04.tar.xz"
 LLVMHome="llvm-10.0.0.obj"
-
+cd ..
 ########
 # Download LLVM binary
 ########
@@ -14,8 +14,9 @@ then
        then
        		echo 'Downloading LLVM binary for MacOS '
       		curl -L $MacLLVM > llvm-mac.tar.xz
-      	 	mkdir ./$LLVMHome && tar -xf "llvm-mac.tar.xz" -C ./$LLVMHome --strip-components 1
-			echo $(pwd)"/"$LLVMHome
+      	 	mkdir ./$LLVMHome 
+		echo 'Unzipping LLVM binary for MacOS '
+		tar -xf "llvm-mac.tar.xz" -C ./$LLVMHome --strip-components 1
 		rm llvm-mac.tar.xz
        fi
 elif [[ $sysOS == "Linux" ]]
@@ -24,15 +25,26 @@ then
        then
        		echo 'Downloading LLVM binary for Ubuntu'
       		wget -c $UbuntuLLVM -O llvm-ubuntu.tar.xz
-      		mkdir ./$LLVMHome && tar -xf "llvm-ubuntu.tar.xz" -C ./$LLVMHome --strip-components 1
-			echo $(pwd)"/"$LLVMHome
+      		mkdir ./$LLVMHome 
+		echo 'Unzipping LLVM binary for Ubuntu'
+		tar -xf "llvm-ubuntu.tar.xz" -C ./$LLVMHome --strip-components 1
 		rm llvm-ubuntu.tar.xz
        fi
 else
 	echo 'not support builds in OS other than Ubuntu and Mac'
 fi
+cd $SVFHOME
 cd ..
 install_path=$(pwd)
-ln -s $install_path/svf-lib $install_path/SVF
-echo $(pwd)
-export LLVM_DIR=$SVFHOME/$LLVMHome
+echo "LLVM_DIR=$install_path/$LLVMHome"
+if [[ $sysOS == "Darwin" ]]
+then 
+ln -s $install_path/svf-lib/SVF-osx $install_path/SVF
+echo "SVF_DIR=$install_path/SVF/"
+echo -e "Build your own project with the following cmake command:\n cmake -DSVF_DIR=$install_path/SVF/SVF-osx -DLLVM_DIR=$install_path/$LLVMHome"
+elif [[ $sysOS == "Linux" ]]
+then 
+ln -s $install_path/svf-lib/SVF-linux $install_path/SVF
+echo "SVF_DIR=$install_path/SVF/"
+echo -e "Build your own project with the following cmake command:\n cmake -DSVF_DIR=$install_path/SVF/SVF-linux -DLLVM_DIR=$install_path/$LLVMHome"
+fi
